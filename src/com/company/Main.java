86d5aa8e6d34600a73e0c2516af12609d5ac2a25
mariya <mariyascott136@gmail.com>
2019-02-11 16:38:02 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        system.out.println("Hi! My name is Mariya Scott");
+        system.out.println("I like to read! one of my favorites is harry potter");
+        system.out.println("I love to read! I been reading since I was a child!");
+        system.out.println("I am hoping to read more books for fun in the future!");
+
+    }
+}
